9a352e9a0f433e817f0eda089e545c52632ee880
Manik Aggarwal <manikaggarwal993@gmail.com>
2021-12-29 19:09:26 +0530

    Intersection of 2 arrays

---
+++ itersection-of-two-array.cpp
@@ -0,0 +1,17 @@
+vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
+{
+	int i=0, j=0;
+    vector <int> v;
+    
+    while(i<n && j<m){
+        if(arr1[i] == arr2[j]){
+            v.push_back(arr1[i]);
+            i++;
+            j++;
+        }
+        else if(arr1[i] < arr2[j]) i++;
+        else j++;
+    }
+    
+    return v;
+}
