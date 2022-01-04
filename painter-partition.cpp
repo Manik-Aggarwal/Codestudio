e1228fbeaf6b27b8f753b654d2f213c2bef4aa30
Manik Aggarwal <manikaggarwal993@gmail.com>
2022-01-04 18:49:16 +0530

    Painer's partition code added

---
+++ painter-partition.cpp
@@ -0,0 +1,39 @@
+bool isPossible(vector<int> &boards, int n, int m, int mid){
+    int studentCount = 1;
+    int pageSum = 0;
+    for(int i=0; i<n; i++){
+        if(pageSum + boards[i] <= mid){
+            pageSum += boards[i];
+        }
+        else{
+            studentCount++;
+            if(studentCount > m || boards[i] > mid) return false;
+            pageSum = boards[i];
+        }
+    }
+    return true;
+}
+
+int findLargestMinDistance(vector<int> &boards, int k)
+{
+    int s = 0;
+    int sum = 0;
+    int n = boards.size();
+    int m = k;
+    
+    for(int i=0; i<n; i++) sum += boards[i];
+    
+    int e = sum;
+    int ans = -1;
+    int mid = s + (e-s)/2;
+    
+    while(s<=e){
+        if(isPossible(boards,n,m,mid)){
+            ans = mid;
+            e = mid - 1;
+        }
+        else s = mid + 1;
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
