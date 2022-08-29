bfee9ae8952749089efa78661dfe9fcd6fe2a6cb
Manik Aggarwal <manikaggarwal993@gmail.com>
2022-08-29 17:04:16 +0530

    Folder Updated

--- Partition with given difference/count-diff-set.cpp
+++ Partition with given difference/count-diff-set.cpp
@@ -1,4 +1,6 @@
-///////////////////////////Memoization////////////////////////
+int mod = (int)(1e9+7);
+
+//////////////////////Memoization//////////////////////////
 int f(int ind, int target, vector<int>& arr, vector<vector<int>> &dp){
     if(ind == 0){
         if(target==0 && arr[0]==0) return 2;
@@ -13,18 +15,26 @@ int f(int ind, int target, vector<int>& arr, vector<vector<int>> &dp){
     int taken = 0;
     if(arr[ind]<=target) taken = f(ind-1,target-arr[ind],arr,dp);
         
-    return dp[ind][target] = notTaken + taken;
+    return dp[ind][target] = (notTaken + taken) % mod;
 }
 
-int findWays(vector<int> &num, int tar)
-{
-    int n = num.size();
-    vector<vector<int>> dp(n,vector<int>(tar+1,-1));
-    return f(n-1, tar, num, dp);
+int solve(vector<int>& arr, int target){
+    int n = arr.size();
+    vector<vector<int>> dp(n,vector<int>(target+1,-1));
+    return f(n-1, target, arr, dp);
 }
 
-///////////////////////Tabulation///////////////////////////
-int findWays(vector<int> &num, int tar){
+int countPartitions(int n, int d, vector<int> &arr) {    
+    int totalsum=0;
+    for(auto it : arr)
+        totalsum +=it;
+    if(totalsum-d < 0 || (totalsum-d) % 2 ) return false;
+
+    return solve(arr, (totalsum-d) / 2);
+}
+
+////////////////////////Tabulation////////////////////////
+int f(vector<int> &num, int tar){
      int n = num.size();
     vector<vector<int>> dp(n,vector<int>(tar+1,0));
     if(num[0] == 0) dp[0][0] = 2;  // 2 cases -pick and not pick
@@ -38,13 +48,21 @@ int findWays(vector<int> &num, int tar){
             int taken = 0;
             if(num[ind]<=target) taken = dp[ind-1][target-num[ind]];
         
-            dp[ind][target]= notTaken + taken;
+            dp[ind][target]= (notTaken + taken)%mod;
         }
     }
     return dp[n-1][tar];
 }
 
-///////////////////////////Space Optmization///////////////////////
+int countPartitions(int n, int d, vector<int>& arr){
+    int totSum = 0;
+    for(int i=0; i<n;i++) totSum += arr[i];
+    if(totSum-d <0 || (totSum-d)%2 ) return 0;
+    
+    return f(arr,(totSum-d)/2);
+}
+
+/////////////////////Space Optimisation//////////////////////////
 int findWays(vector<int> &num, int tar){
      int n = num.size();
     vector<int> prev(tar+1,0);
@@ -60,9 +78,17 @@ int findWays(vector<int> &num, int tar){
             int taken = 0;
             if(num[ind]<=target) taken = prev[target-num[ind]];
         
-            cur[target] = notTaken + taken;
+            cur[target] = (notTaken + taken)%mod;
         }
         prev = cur;
     }
     return prev[tar];
 }
+
+int countPartitions(int n, int d, vector<int>& arr){
+    int totSum = 0;
+    for(int i=0; i<n;i++) totSum += arr[i];    
+    if(totSum-d <0 || (totSum-d)%2 ) return 0;
+    return findWays(arr,(totSum-d)/2);
+}
+  
